a2dbc570fc1d2fe46ad58c324731c579b281ad9b
Dawid Stoga <dawid.stoga@gmail.com>
2019-10-10 20:43:18 +0200

    Add project files.

---
+++ WebApplication8/ProxyHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using ProxyKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ProxyKitDemo
+{
+    public class ProxyHandlers : IProxyHandler
+    {
+
+        public Task<HttpResponseMessage> HandleProxyRequest(HttpContext httpContext)
+        {
+            string host = "https://fakerestapi.azurewebsites.net";
+            var forwardContext = httpContext.ForwardTo(host);
+            forwardContext.UpstreamRequest.RequestUri = new Uri("https://fakerestapi.azurewebsites.net/api/books");
+            var response = forwardContext.Send();
+            return response;
+        }
+
+        public Task<HttpResponseMessage> HandleProxyRequestForValues(HttpContext httpContext)
+        {
+            string host = "https://fakerestapi.azurewebsites.net";
+            var forwardContext = httpContext.ForwardTo(host);
+            forwardContext.UpstreamRequest.RequestUri = new Uri("https://fakerestapi.azurewebsites.net/api/Authors");
+            var response = forwardContext.Send();
+            return response;
+        }
+    }
+}
---
+++ WebApplication8/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ProxyKit;
+using ProxyKitDemo;
+
+namespace WebApplication8
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddHttpClient("proxyClient", c =>
+            {
+                c.DefaultRequestHeaders.Add("Accept", "application/json");
+                c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            });
+
+            services.AddProxy(httpBuilder =>
+            {
+
+                httpBuilder.ConfigureHttpClient(c => c.Timeout = TimeSpan.FromSeconds(5));
+            });
+            services.AddSingleton<ProxyHandlers>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.MapWhen(Predicate, GoProxyKit);
+            app.RunProxy(new ProxyHandlers().HandleProxyRequestForValues);
+            app.UseMvc();
+        }
+
+        private void GoProxyKit(IApplicationBuilder obj)
+        {
+            obj.RunProxy<ProxyHandlers>();
+        }
+
+        private bool Predicate(HttpContext ctx)
+        {
+            return ctx.Request.Path.Value.Contains("api/Books",StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
